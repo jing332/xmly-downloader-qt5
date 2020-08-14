dcacf1f141b680d95282043a8223128d3afc9609
jing <3033784236@qq.com>
2020-08-14 08:36:02 +0800

    将脚本移动到ci文件夹

---
+++ ci/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export MAKE="make"
+if [ "Windows_NT" = "$OS" ];then export MAKE="mingw32-make"; fi
+
+export GOARCH=386
+export CGO_ENABLED=1
+
+cd ./src/cgoqt
+go build -buildmode=c-archive -o xmlydownloader.a
+cd ../../
+
+mkdir build
+cd build
+qmake CONFIG+=release ../src/xmly-downloader-qt5.pro
+$MAKE -j2
+cd ../
